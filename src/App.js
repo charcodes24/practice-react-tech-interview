d78920e0ccd0c4c5a533be25fa216b8cd53bd9e7
charcodes24 <charcodes24@gmail.com>
2021-11-24 17:12:08 -0800

    HhandleSubmit function that successfully appends tags to DOM

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
 
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('')
+
+  function handleInput(e) {
+    setInput(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setInput("");
+    let container = document.getElementById('tags')
+    let tag = document.createElement('button')
+    tag.innerHTML = input
+    container.appendChild(tag)
+  }
+
+
   return (
     <div className="App">
-      <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleInput} type="text" />
+      </form>
       <div id="tags">
-        
+
       </div>
       <button>Clear</button>
     </div>
